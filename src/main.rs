a552b3567a8d75fcf6f6aa95eb080cd7126c5b8f
kevin lutzer <kevinlutzer@proton.me>
2025-08-03 17:12:11 -0600

    cleanup and merge

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,15 @@ use clap::Parser;
 use cli::{Cli, Commands};
 use mqtt::{build_client, connect_client, publish, subscribe, ClientConfig};
 
-use std::path::PathBuf;
+use std::{fs::File, io::Read, path::PathBuf};
 #[allow(deprecated)]
-use std::{
-    env::{home_dir, var},
-    process::exit,
-};
+use std::{env::{var, home_dir}, process::exit};
 use tokio::main;
 
 pub mod cli;
 pub mod mqtt;
 
-const CONFIG_FILE_NAME: &str = "rmqttconfig";
-const SNAP_COMMON: &str = "SNAP_COMMON";
+const CONFIG_FILE_NAME: &str = ".rmqttconfig";
 
 /// Exit code when the program fails to parse the arguments. This means that the user
 /// provided invalid arguments or the arguments were not provided at all.
@@ -33,18 +29,18 @@ const ERR_FAILED_COMMAND: i32 = 4;
 /// TODO - add support for Windows home directory and SNAP_COMMON in the future
 fn load_config_file() {
     // Get a path ref to the config file
-    #[allow(deprecated)]
-    let config_file_path = home_dir().map(|path| path.join(CONFIG_FILE_NAME));
+
+    let config_file_path = var("SNAP_USER_COMMON")
+        .or_else(|_| var("HOME"))
+        .map(|path| {
+            println!("HOME: {path}");   
+            let pb = PathBuf::from(path);
+            return pb.join(CONFIG_FILE_NAME);
+        });
 
     // If the config file exists, load the environment variables from it
-    if let Some(cf) = config_file_path {
-        dotenvy::from_path(&cf).ok();
-
-        // Not check the snap common directory
-    } else {
-        if let Ok(snap_common) = var(SNAP_COMMON) {
-            dotenvy::from_path(PathBuf::from(snap_common).join(CONFIG_FILE_NAME)).ok();
-        }
+    if let Ok(cf) = config_file_path {
+        dotenvy::from_path(&cf).expect("ASDASDASD");
     }
 }
 
@@ -54,19 +50,19 @@ async fn main() {
 
     // Parse the CLI, ::try_parse() will return an error if the arguments are invalid or if `--help` is passed
     let cli = Cli::try_parse().unwrap_or_else(|e| {
-        eprintln!("{}", e);
+        eprintln!("{e}");
         exit(ERR_PARSING_ARGS);
     });
 
     let config = ClientConfig::new(cli.host, cli.port);
     let mut client = build_client(config).unwrap_or_else(|e| {
-        eprintln!("Error creating MQTT client: {}", e);
+        eprintln!("Error creating MQTT client: {e}");
         exit(ERR_CREATING_MQTT_CLIENT);
     });
 
     // Connect to the MQTT server
     connect_client(&client).await.unwrap_or_else(|e| {
-        eprintln!("Error connecting to MQTT server: {}", e);
+        eprintln!("Error connecting to MQTT server: {e}");
         exit(ERR_CONNECTING_MQTT_BROKER);
     });
 
@@ -80,7 +76,7 @@ async fn main() {
         Commands::Sub {} => subscribe(&mut client, topic, qos).await,
     }
     .unwrap_or_else(|cmd_err| {
-        eprintln!("{}", cmd_err);
+        eprintln!("{cmd_err}");
         exit(ERR_FAILED_COMMAND);
     });
 }
