48111a6d1beb0d60574e738169dabb0ef46f0c54
Kevin Lutzer <kevinlutzer@proton.me>
2025-08-16 13:51:41 -0600

    add snap hooks WIP and justfile for snap packaging

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,8 @@ const ERR_FAILED_COMMAND: i32 = 4;
 /// TODO - add support for Windows home directory and SNAP_COMMON in the future
 fn load_config_file() {
     // Get a path ref to the config file
-
-    let config_file_path = var("SNAP_USER_COMMON")
+    // If we have a SNAP_DATA file, we know we are in snap confinement
+    let config_file_path = var("SNAP_DATA")
         .or_else(|_| var("HOME"))
         .map(|path| {
             let pb = PathBuf::from(path);
